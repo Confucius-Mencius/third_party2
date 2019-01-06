a6902ab041ef9cf96c88ce5e02617d13e8af94fd
brenthuang <guang11cheng@qq.com>
2019-01-06 21:40:40 +0800

    udt改名

--- build/build_udt.sh
+++ build/build_udt.sh
@@ -22,9 +22,9 @@ fi
 . ${SCRIPT_PATH}/build_type.sh
 . ${SCRIPT_PATH}/common.sh
 
-echo "build libudt..."
+echo "build udt..."
 
-cd ${LIBUDT_SRC_DIR}
+cd ${UDT_SRC_DIR}
 
 make clean
 
@@ -37,14 +37,14 @@ else
     exit 1
 fi
 
-if [ ! -d ${LIBUDT_INSTALL_DIR}/include ]; then
-    sudo mkdir -p ${LIBUDT_INSTALL_DIR}/include
+if [ ! -d ${UDT_INSTALL_DIR}/include ]; then
+    sudo mkdir -p ${UDT_INSTALL_DIR}/include
 fi
 
-if [ ! -d ${LIBUDT_INSTALL_DIR}/lib ]; then
-    sudo mkdir -p ${LIBUDT_INSTALL_DIR}/lib
+if [ ! -d ${UDT_INSTALL_DIR}/lib ]; then
+    sudo mkdir -p ${UDT_INSTALL_DIR}/lib
 fi
 
-sudo cp -f ./src/udt.h ${LIBUDT_INSTALL_DIR}/include/
-sudo cp -f ./src/libudt.a ${LIBUDT_INSTALL_DIR}/lib/
-sudo cp -f ./src/libudt.so ${LIBUDT_INSTALL_DIR}/lib/
+sudo cp -f ./src/udt.h ${UDT_INSTALL_DIR}/include/
+sudo cp -f ./src/libudt.a ${UDT_INSTALL_DIR}/lib/
+sudo cp -f ./src/libudt.so ${UDT_INSTALL_DIR}/lib/
